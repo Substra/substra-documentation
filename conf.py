eae87d1f34fdb4f338bae0bbfd6931e31f5cd51c
natct10 <nathanael.cretin@substra.org>
2020-03-25 14:11:22 +0100

    Clean sys path & rm old gif

--- conf.py
+++ conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath("../substra"))
-sys.path.append(os.path.abspath("../.github"))
 
 # -- Project information -----------------------------------------------------
 
