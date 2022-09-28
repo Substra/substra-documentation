bf08779f11476460a2d392d3d0baa6c4705b1705
Fabien Gelus <fabien.gelus@owkin.com>
2022-09-28 14:51:25 +0200

    fix: Environment variable not set properly in Substrafl example
    Signed-off-by: Fabien Gelus <fabien.gelus@owkin.com>

--- substrafl_examples/strategies_examples/plot_substrafl_fedavg.py
+++ substrafl_examples/strategies_examples/plot_substrafl_fedavg.py
@@ -60,9 +60,12 @@ from torchvision.datasets import MNIST
 
 from substra import Client
 
-N_CLIENTS = 2
+# Choose the subprocess mode to locally simulate the FL process
+DEBUG_SPAWNER = "subprocess"
+os.environ["DEBUG_SPAWNER"] = DEBUG_SPAWNER
 
 # Create the substra clients
+N_CLIENTS = 2
 clients = [Client(debug=True) for _ in range(N_CLIENTS)]
 clients = {client.organization_info().organization_id: client for client in clients}
 
@@ -72,9 +75,6 @@ ORGS_ID = list(clients.keys())
 # The org id on which your computation tasks are registered
 ALGO_ORG_ID = ORGS_ID[1]
 
-# Choose the subprocess mode to locally simulate the FL process
-DEBUG_SPAWNER = "subprocess"
-os.environ["DEBUG_SPAWNER"] = DEBUG_SPAWNER
 
 # Create the temporary directory for generated data
 (pathlib.Path.cwd() / "tmp").mkdir(exist_ok=True)
