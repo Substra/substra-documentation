6e02a6facf8d9eb740f884b14e1f4ab6ca8a2d11
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-02-09 18:37:00 +0100

    rename
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,9 +32,9 @@ if os.environ.get("READTHEDOCS_VERSION_TYPE") == "tag":
     SUBSTRAFL_VERSION = "0.33.0"
 
 else:
-    SUBSTRA_VERSION = "rename-algo-to-function"
+    SUBSTRA_VERSION = "main"
     TOOLS_VERSION = "main"
-    SUBSTRAFL_VERSION = "rename-algo-to-function"
+    SUBSTRAFL_VERSION = "main"
 
 print(
     f"Versions of the components used:"
