a6eab70caa1b7635abc641b6ce92c55734553131
Olivier Léobal <olivier.leobal@owkin.com>
2023-02-23 16:29:23 +0100

    chore: update k3-create.sh
    Signed-off-by: Olivier Léobal <olivier.leobal@owkin.com>

--- docs/source/contributing/local-deployment/k3-create.sh
+++ docs/source/contributing/local-deployment/k3-create.sh
@@ -10,7 +10,7 @@ fi
 k3d cluster delete || echo 'No cluster'
 mkdir -p /tmp/org-1
 mkdir -p /tmp/org-2
-k3d cluster create --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer --k3s-arg "--no-deploy=traefik,metrics-server@server:*" --volume /tmp/org-1:/tmp/org-1 --volume /tmp/org-2:/tmp/org-2
+k3d cluster create --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer --k3s-arg "--disable=traefik,metrics-server@server:*" --volume /tmp/org-1:/tmp/org-1 --volume /tmp/org-2:/tmp/org-2
 
 # Patch and install nginx-ingress
 curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml > /tmp/deploy.yaml
