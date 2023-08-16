bed4edb53b645d26569a7374426d0e87ea798e12
SdgJlbl <sarah.diot-girard@owkin.com>
2023-08-16 17:08:27 +0200

    chore: deactivate binder (#340)
    Signed-off-by: SdgJlbl <sarah.diot-girard@owkin.com>

--- substrafl_examples/go_further/run_mnist_cyclic.py
+++ substrafl_examples/go_further/run_mnist_cyclic.py
@@ -18,12 +18,7 @@ This example shows an implementation of the CyclicTorchAlgo using
 
 This example does not use a deployed platform of Substra and runs in local mode.
 
-To run this example, you have two options:
-
-- **Recommended option**: use a hosted Jupyter notebook. With this option you don't have to install anything, just run the notebook.
-  To access the hosted notebook, scroll down at the bottom of this page and click on the **Launch Binder** button.
-- **Run the example locally**. To do that you need to download and unzip the assets needed to run it in the same
-  directory as used this example.
+To run this example, you need to download and unzip the assets needed to run it in the same directory as used this example.
 
    .. only:: builder_html or readthedocs
 
