9891578cd772f356c18882cf89e27db9c1525c92
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-09-14 14:23:43 +0200

    fix: example
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- examples/substrafl/go_further/run_mnist_cyclic.py
+++ examples/substrafl/go_further/run_mnist_cyclic.py
@@ -785,7 +785,7 @@ compute_plan = execute_experiment(
 # *******************
 
 # The results will be available once the compute plan is completed
-client_0.wait_compute_plan(compute_plan.key)
+clients[ALGO_ORG_ID].wait_compute_plan(compute_plan.key)
 
 # %%
 # List results
