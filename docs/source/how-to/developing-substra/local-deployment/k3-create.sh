58cbca087e50f4bf07d905902d47230f2cafd280
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-09-14 14:33:16 +0200

    feat: allow three orgs with skaffold
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- docs/source/how-to/developing-substra/local-deployment/k3-create.sh
+++ docs/source/how-to/developing-substra/local-deployment/k3-create.sh
@@ -10,14 +10,15 @@ fi
 k3d cluster delete || echo 'No cluster'
 mkdir -p /tmp/org-1
 mkdir -p /tmp/org-2
-k3d cluster create --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer --k3s-arg "--disable=traefik,metrics-server@server:*" --volume /tmp/org-1:/tmp/org-1 --volume /tmp/org-2:/tmp/org-2
+mkdir -p /tmp/org-3
+k3d cluster create --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer --k3s-arg "--disable=traefik,metrics-server@server:*" --volume /tmp/org-1:/tmp/org-1 --volume /tmp/org-2:/tmp/org-2 --volume /tmp/org-3:/tmp/org-3
 
 # Patch and install nginx-ingress
 curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml > /tmp/deploy.yaml
 $SED_EXEC -i 's/        - --publish-status-address=localhost/        - --publish-status-address=localhost\n        - --enable-ssl-passthrough/g' /tmp/deploy.yaml
 $SED_EXEC -i "/ingress-ready: \"true\"/d" /tmp/deploy.yaml
 kubectl apply -f /tmp/deploy.yaml
-
 kubectl create ns orderer
 kubectl create ns org-1
 kubectl create ns org-2
+kubectl create ns org-3
