dd4988219e67236f3870308c4ef87f97c693aa1d
Guilhem Barthés <guilhem.barthes@owkin.com>
2024-04-16 17:45:52 +1000

    feat: update `k3-create.sh`
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- docs/source/how-to/developing-substra/local-deployment/k3-create.sh
+++ docs/source/how-to/developing-substra/local-deployment/k3-create.sh
@@ -11,7 +11,13 @@ k3d cluster delete || echo 'No cluster'
 mkdir -p /tmp/org-1
 mkdir -p /tmp/org-2
 mkdir -p /tmp/org-3
-k3d cluster create --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer --k3s-arg "--disable=traefik,metrics-server@server:*" --volume /tmp/org-1:/tmp/org-1 --volume /tmp/org-2:/tmp/org-2 --volume /tmp/org-3:/tmp/org-3
+args=()
+
+if [ "$HARBOR_CERT_PATH" ]; then
+   args+=("--volume" "${HARBOR_CERT_PATH}:/etc/ssl/certs/harbor.crt")
+fi
+
+k3d cluster create --api-port 127.0.0.1:6443 -p 80:80@loadbalancer -p 443:443@loadbalancer --k3s-arg "--disable=traefik,metrics-server@server:*" --volume /tmp/org-1:/tmp/org-1 --volume /tmp/org-2:/tmp/org-2 --volume /tmp/org-3:/tmp/org-3 "${args[@]}"
 
 # Patch and install nginx-ingress
 curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml > /tmp/deploy.yaml
