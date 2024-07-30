ff7592ea2aebc228bab27129eb7925294426f4ee
ThibaultFy <thibault.fouqueray@gmail.com>
2024-07-30 11:22:49 +0200

    fix: add build os info in readthedocs.yaml
    Signed-off-by: ThibaultFy <thibault.fouqueray@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,16 +44,16 @@ repo = git.Repo(search_parent_directories=True)
 current_commit = repo.head.commit
 tagged_commits = [tag.commit for tag in repo.tags]
 
-if os.environ.get("READTHEDOCS_VERSION_TYPE") == "tag" or current_commit in tagged_commits:
+if True:  # os.environ.get("READTHEDOCS_VERSION_TYPE") == "tag" or current_commit in tagged_commits:
     # Index 0 means latest release
     SUBSTRA_VERSION = compat_table["releases"][0]["components"]["substra"]["version"]
     TOOLS_VERSION = compat_table["releases"][0]["components"]["substra-tools"]["version"]
     SUBSTRAFL_VERSION = compat_table["releases"][0]["components"]["substrafl"]["version"]
 
-else:
-    SUBSTRA_VERSION = "main"
-    TOOLS_VERSION = "main"
-    SUBSTRAFL_VERSION = "main"
+# else:
+#     SUBSTRA_VERSION = "main"
+#     TOOLS_VERSION = "main"
+#     SUBSTRAFL_VERSION = "main"
 
 print(
     f"Versions of the components used:"
